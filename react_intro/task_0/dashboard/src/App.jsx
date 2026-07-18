ad2d96e8de63c9cb8d5c6594d71d23d9496581c6
Sabir <12734@holbertonstudents.com>
2026-07-19 00:58:31 +0400

    some stuff

--- react_intro/task_0/dashboard/src/App.jsx
+++ react_intro/task_0/dashboard/src/App.jsx
@@ -1,25 +1,26 @@
-import "./App.css";
-import holbertonLogo from "./assets/holberton-logo.jpg";
+import './App.css';
+import holbertonLogo from './assets/holberton-logo.jpg';
 
 function App() {
   const currentYear = new Date().getFullYear();
 
   return (
     <div className="App">
-      <div className="App-header">
+      <header className="App-header">
         <img src={holbertonLogo} alt="holberton logo" />
-        <h1>School dashboard</h1>
-      </div>
+        <h1>School Dashboard</h1>
+      </header>
 
       <div className="App-body">
         <p>Login to access the full dashboard</p>
       </div>
 
       <div className="App-footer">
-        <p>Copyright {currentYear} - Holberton School</p>
+        <p>Copyright {currentYear} - holberton School</p>
       </div>
     </div>
   );
 }
+
 
 export default App;
